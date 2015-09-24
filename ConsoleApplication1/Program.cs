f7d2672e14438077c10f16283f9652e0bbf97e2f
Andres Da Costa <andrescostaribeiro16@gmail.com>
2015-09-24 16:58:34 +0200

    24/09/2015
    24/09/2015 Jorge andres da costa

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            String palabra = null;
+            
+            palabra =  Console.ReadLine();
+            numero = int.Parse(palabra);
+
+            if (numero > 0) {
+
+                Console.WriteLine("el numero es positivo");
+
+            }
+            else if (numero == 0)
+            {
+
+                Console.WriteLine("el numero no es negativo ni positivo");
+
+            }
+            else {
+
+                Console.WriteLine("el numero es negativo");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
